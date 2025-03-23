04de359b2a6675c606a0a0e85b03fe1893c6fd9e
Bruno C D Ambrosio <bruno.ambrosio@ifood.com.br>
2025-03-23 00:10:39 -0300

    Initial Commit

---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by Bruno Ambrosio on 23/03/25.
+//
+
+import Testing
+
+struct ExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
